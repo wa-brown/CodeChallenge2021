8edf023b8981092d8167cf9a8e9a09ca1afbfed8
Warren Brown <wa.brown@reply.com>
2021-03-11 18:54:50 +0000

    done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import building
 import antenna
 
 def main():
-    fileName = 'data_scenarios_a_example.in'
+    fileName = 'data_scenarios_f_tokyo.in'
     buildings = []
     antennas = []
 
@@ -12,7 +12,8 @@ def main():
         w, h = lines[0].strip().split(" ")
         grid = [[0]* int(w) for _ in range(int(h))]
         n, _, r = lines[1].strip().split(" ")
-        for line in lines[2: int(n)]:
+        print(f'n: {n}')
+        for line in lines[2: int(n) + 1]:
             x, y, latency, connection_speed = line.strip().split(" ")
             buildings.append(building.Building(x, y, latency, connection_speed))
             grid[int(y)][int(x)] = 1
@@ -23,27 +24,41 @@ def main():
             id += 1
 
     # process
-    antennas = process(h, w, grid, antennas)
-    for a in antennas:
-        print(a.x)
+    antennas = process(h, w, buildings, antennas)
 
     # output
     outfile = fileName.split('.')[0] + '.out'
     with open(outfile, 'w') as writer:
         writer.write(f'{len(antennas)}\n')
         for an in antennas:
+<<<<<<< Updated upstream
             writer.write(f'{an.id} {an.x} {an.y}\n')
 
+=======
+            try:
+                writer.write(f'{an.id} {an.x} {an.y}\n')
+            except:
+                print('oops')
+    
+>>>>>>> Stashed changes
 
-def process(h, w, grid, antennas):
+def process(h, w, buildings, antennas):
     count = 0
-    for y in range(int(h)):
-        for x in range(int(w)):
-            if count > len(antennas):
-                return antennas
-            if grid[y][x] is 1:
-                antennas[count].set_coords(x, y)
-                count += 1
+    # for y in range(int(h)):
+    #     for x in range(int(w)):
+    #         if count > len(antennas):
+    #             return antennas
+    #         if grid[y][x] == 1:
+    #             antennas[count].set_coords(x, y)
+    #             count += 1
+
+    print(len(buildings))
+    for b in buildings:
+        antennas[count].set_coords(b.x, b.y)
+        count += 1
+        if count == len(antennas):
+            print('returning')
+            return antennas
     return antennas
 
 if __name__ == "__main__":
