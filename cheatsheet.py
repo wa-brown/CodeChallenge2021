a1bdac2d48e91f873a6b65720278931ee66452c2
Stelios Moschos <s.moschos@reply.com>
2021-03-11 15:13:52 +0000

    OOP example

--- cheatsheet.py
+++ cheatsheet.py
@@ -18,4 +18,32 @@ with open('dog_breeds.txt', 'r') as reader:
 # ----------------------------------------------------------------------------------------
 
 
-# test commit
+# OOP in python
+
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance method
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+
+    # Another instance method
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+
+# call object class
+>>> miles = Dog("Miles", 4)
+
+>>> miles.description()
+'Miles is 4 years old'
+
+>>> miles.speak("Woof Woof")
+'Miles says Woof Woof'
+
+>>> miles.speak("Bow Wow")
+'Miles says Bow Wow'
