40b6206872b4d5d4ce59f2f0106c8501c550520d
Warren Brown <wa.brown@reply.com>
2021-03-11 16:13:47 +0000

    main

---
+++ antenna.py
@@ -0,0 +1,4 @@
+class Antenna():
+    def __init__(self, range, connection_speed):
+        self.range = range
+        self.connection_speed = connection_speed
---
+++ building.py
@@ -0,0 +1,6 @@
+class Building():
+    def __init__(self, x, y, latency, connection_speed):
+        self.x = x
+        self.y = y
+        self.latency = latency
+        self.connection_speed = connection_speed
